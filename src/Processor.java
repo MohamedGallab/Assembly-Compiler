a32ba8b8b460ac9d5e5b6e493752548e14d12acf
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2022-05-19 18:53:49 +0200

    fixed carry flag and rewrote decode

--- src/Processor.java
+++ src/Processor.java
@@ -2,10 +2,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
 
 public class Processor {
-	String[] instructionMemory = new String[1024];
-	String[] dataMemory = new String[2048];
+	Short[] instructionMemory = new Short[1024];
+	byte[] dataMemory = new byte[2048];
 	byte[] registers = new byte[64];
 	byte SREG;
 	short PC = 0;
@@ -13,7 +15,6 @@ public class Processor {
 
 	public void parse(String pathToFile) throws IOException {
 		int parserIterator = 0;
-		String result = "";
 
 		File file = new File(pathToFile);
 		BufferedReader br = new BufferedReader(new FileReader(file));
@@ -21,94 +22,105 @@ public class Processor {
 		String currentLine[];
 
 		while ((st = br.readLine()) != null) {
+			instructionMemory[parserIterator] = 0;
 			currentLine = st.split(" ");
 			switch (currentLine[0]) {
 			case "Add":
-				result = Integer.toBinaryString(0);
+				instructionMemory[parserIterator] = 0 << 12;
 				break;
 			case "SUB":
-				result = Integer.toBinaryString(1);
+				instructionMemory[parserIterator] = 1 << 12;
 				break;
 			case "MUL":
-				result = Integer.toBinaryString(2);
+				instructionMemory[parserIterator] = 2 << 12;
 				break;
 			case "LDI":
-				result = Integer.toBinaryString(3);
+				instructionMemory[parserIterator] = 3 << 12;
 				break;
 			case "BEQZ":
-				result = Integer.toBinaryString(4);
+				instructionMemory[parserIterator] = 4 << 12;
 				break;
 			case "AND":
-				result = Integer.toBinaryString(5);
+				instructionMemory[parserIterator] = 5 << 12;
 				break;
 			case "OR":
-				result = Integer.toBinaryString(6);
+				instructionMemory[parserIterator] = 6 << 12;
 				break;
 			case "JR":
-				result = Integer.toBinaryString(7);
+				instructionMemory[parserIterator] = 7 << 12;
 				break;
 			case "SLC":
-				result = Integer.toBinaryString(8);
+				instructionMemory[parserIterator] = (short) (8 << 12);
 				break;
 			case "SRC":
-				result = Integer.toBinaryString(9);
+				instructionMemory[parserIterator] = (short) (9 << 12);
 				break;
 			case "LB":
-				result = Integer.toBinaryString(10);
+				instructionMemory[parserIterator] = (short) (10 << 12);
 				break;
 			case "SB":
-				result = Integer.toBinaryString(11);
+				instructionMemory[parserIterator] = (short) (11 << 12);
 				break;
 			}
-			instructionMemory[parserIterator] = extend(result, 4)
-					+ extend(Integer.toBinaryString(Integer.parseInt(currentLine[1]) & 0x3f), 6)
-					+ extend(Integer.toBinaryString(Integer.parseInt(currentLine[2]) & 0x3f), 6);
+			instructionMemory[parserIterator] = (short) (instructionMemory[parserIterator]
+					+ ((Integer.parseInt(currentLine[1]) & 0x3f) << 6) + (Integer.parseInt(currentLine[2]) & 0x3f));
 			parserIterator++;
 		}
-
 	}
 
-	public String extend(String s, int finalSize) {
-		while (s.length() < finalSize)
-			s = "0" + s;
-		return s;
-	}
-
-	public String fetch() {
+	public Short fetch() {
 		return instructionMemory[PC++];
 	}
 
-	public int[] decode(String instruction) {
-		int[] decodedInstruction = new int[3];
-		decodedInstruction[0] = Integer.parseInt(instruction.substring(0, 4), 2);
-		decodedInstruction[1] = Integer.parseInt(instruction.substring(4, 10), 2);
-		decodedInstruction[2] = Integer.parseInt(instruction.substring(10, 16), 2);
+	public int[] decode(short instruction) {
+		int[] decodedInstruction = new int[7];
+
+		decodedInstruction[0] = (instruction & 0b1111000000000000) >> 12;	// opcode			
+		decodedInstruction[1] = registers[(instruction & 0b0000111111000000) >> 6];	// r1 value
+		decodedInstruction[2] = registers[instruction & 0b0000000000111111];	// r2 value
+		decodedInstruction[3] = (short) (instruction & 0b0000000000111111);	// imm
+		decodedInstruction[4] = (short) (instruction & 0b0000000000111111);	// signedImmediate
+		if(((instruction & 32) >> 5) == 1)
+			decodedInstruction[4] = (short) ((instruction & 0b0000000000111111) - 64);
+		decodedInstruction[5] = (instruction & 0b0000111111000000) >> 6;	// r1 address
+		decodedInstruction[6] = instruction & 0b0000000000111111;			// r2 address
+		
+
 		return decodedInstruction;
 	}
 
-	public void execute(int[] decodedInstruction) {
+	public boolean execute(int[] decodedInstruction) {
 		SREG = 0;
-		int R1 = decodedInstruction[1];
-		int R2 = decodedInstruction[2];
+		byte R1 = (byte) decodedInstruction[1];
+		byte R2 = (byte) decodedInstruction[2];
+		int UnSignedIMM = decodedInstruction[3];
+		int SignedIMM = decodedInstruction[4];
+		int R1Address = decodedInstruction[5];
+		int R2Address = decodedInstruction[6];
+		
+		
 		byte result = 0;
+		String oldR1 = registers[R1Address] + "";
+		String oldSREG = Integer.toBinaryString(SREG);
+		System.out.println("------------------------------------------");
 		// 7 6 5 4 3 2 1 0
 		// 0 0 0 C V N S Z
 		switch (decodedInstruction[0]) {
 
 		// ADD
 		case 0:
-			result = (byte) (registers[R1] + registers[R2]);
-			registers[R1] = result;
+			result = (byte) (R1 + R2);
+			
 			// C
-			if (Byte.toUnsignedInt(registers[R1]) + Byte.toUnsignedInt(registers[R2]) > Byte.MAX_VALUE) {
+			if (((Byte.toUnsignedInt(R1) + Byte.toUnsignedInt(R2))&0b100000000) == 0b100000000) {
 				SREG |= (1 << 4);
 			}
 			// V
-			if (registers[R1] >= 0 && registers[R2] >= 0) {
+			if (R1 >= 0 && R2 >= 0) {
 				if ((byte) (result) < 0)
 					SREG |= (1 << 3);
 			}
-			else if (registers[R1] < 0 && registers[R2] < 0) {
+			else if (R1 < 0 && R2 < 0) {
 				if ((byte) (result) >= 0)
 					SREG |= (1 << 3);
 			}
@@ -120,18 +132,19 @@ public class Processor {
 			// Z
 			if (result == 0)
 				SREG |= 1;
+			registers[R1Address] = result;
 			break;
 
 		// SUB
 		case 1:
-			result = (byte) (registers[R1] - registers[R2]);
-			registers[R1] = result;
+			result = (byte) (R1 - R2);
+			
 			// V
-			if (registers[R1] >= 0 && registers[R2] < 0) {
+			if (R1 >= 0 && R2 < 0) {
 				if ((byte) (result) < 0)
 					SREG |= (1 << 3);
 			}
-			else if (registers[R1] < 0 && registers[R2] >= 0) {
+			else if (R1 < 0 && R2 >= 0) {
 				if ((byte) (result) >= 0)
 					SREG |= (1 << 3);
 			}
@@ -143,118 +156,169 @@ public class Processor {
 			// Z
 			if (result == 0)
 				SREG |= 1;
+			registers[R1Address] = result;
 			break;
 
 		// MUL
 		case 2:
-			result = (byte) (registers[R1] * registers[R2]);
-			registers[R1] = result;
+			result = (byte) (R1 * R2);
+			
 			// N
 			if (result < 0)
 				SREG |= (1 << 2);
 			// Z
 			if (result == 0)
 				SREG |= 1;
+			registers[R1Address] = result;
 			break;
 
 		// LDI
 		case 3:
-			registers[R1] = (byte) R2;
+			registers[R1Address] = (byte) SignedIMM;
 			break;
 
 		// BEQZ
 		case 4:
-			if (registers[R1] == 0)
-				PC += 1 + R2;
-			break;
+			if (R1 == 0) {
+				System.out.println("Old PC value: " + PC);
+				PC += 1 + SignedIMM;
+				System.out.println("New PC value: " + PC);
+
+			}
+			System.out.println("------------------------------------------");
+			return true;
 
 		// AND
 		case 5:
-			result = (byte) (registers[R1] & registers[R2]);
-			registers[R1] = result;
+			result = (byte) (R1 & R2);
+			
 			// N
 			if (result < 0)
 				SREG |= (1 << 2);
 			// Z
 			if (result == 0)
 				SREG |= 1;
+			registers[R1Address] = result;
 			break;
 
 		// OR
 		case 6:
-			result = (byte) (registers[R1] | registers[R2]);
-			registers[R1] = result;
+			result = (byte) (R1 | R2);
+			
 			// N
 			if (result < 0)
 				SREG |= (1 << 2);
 			// Z
 			if (result == 0)
 				SREG |= 1;
+			registers[R1Address] = result;
 			break;
 
 		// JR
 		case 7:
-			PC = (short) (registers[R1] << 8 + registers[R2]);
-			break;
+			System.out.println("Old PC value: " + PC);
+			PC = (short) (R1 << 8 + R2);
+			System.out.println("New PC value: " + PC);
+			System.out.println("------------------------------------------");
+			return true;
 
 		// SLC
 		case 8:
-			result = (byte) (registers[R1] << R2 | registers[R1] >>> 8 - R2);
-			registers[R1] = result;
+			result = (byte) (R1 << UnSignedIMM | R1 >>> 8 - UnSignedIMM);
+			
 			// N
 			if (result < 0)
 				SREG |= (1 << 2);
 			// Z
 			if (result == 0)
 				SREG |= 1;
+			registers[R1Address] = result;
 			break;
 
 		// SRC
 		case 9:
-			result = (byte) (registers[R1] >>> R2 | registers[R1] << 8 - R2);
-			registers[R1] = result;
+			result = (byte) (R1 >>> UnSignedIMM | R1 << 8 - UnSignedIMM);
+			
 			// N
 			if (result < 0)
 				SREG |= (1 << 2);
 			// Z
 			if (result == 0)
 				SREG |= 1;
+			registers[R1Address] = result;
 			break;
 
 		// LB
 		case 10:
-			registers[R1] = (byte) Integer.parseUnsignedInt(dataMemory[R2], 2);
+			registers[R1Address] = dataMemory[UnSignedIMM];
 			break;
 
 		// SB
 		case 11:
-			dataMemory[R2] = Integer.toBinaryString(registers[R1] & 0xff);
-			break;
+			System.out.println("Old Memory Cell " + UnSignedIMM + " value: " + dataMemory[UnSignedIMM]);
+			dataMemory[UnSignedIMM] = R1;
+			System.out.println("New Memory Cell " + UnSignedIMM + " value: " + dataMemory[UnSignedIMM]);
+			System.out.println("------------------------------------------");
+			return false;
 		}
-
+		System.out.println("Old R1 value: " + oldR1);
+		System.out.println("Old SREG value: " + oldSREG);
+		System.out.println("New R1 value: " + registers[R1Address]);
+		System.out.println("New SREG value: " + Integer.toBinaryString(SREG));
+		System.out.println("------------------------------------------");
+		return false;
 	}
 
 	public void run() {
-		String fetchedInstruction = null;
+		Short fetchedInstruction = null;
 		int[] decodedInstruction = null;
-		for (String instruction : instructionMemory) {
-			System.out.println(cycles++);
-			System.out.println("	" + instruction);
-			System.out.println("	" + fetchedInstruction);
-			System.out.println("	" + decodedInstruction);
-			System.out.println();
-			if (instruction == null && fetchedInstruction == null && decodedInstruction == null)
+		boolean isJumped = false;
+		for (Short instruction : instructionMemory) {
+
+			if (instruction == null && fetchedInstruction == null && decodedInstruction == null) {
 				return;
-			if (decodedInstruction != null)
-				execute(decodedInstruction);
-			if (fetchedInstruction != null)
+			}
+			System.out.println("clock: " + cycles++);
+			System.out.println();
+			System.out.println("Execute Stage:");
+			System.out.println("	" + "Input: " + Arrays.toString(decodedInstruction));
+			if (decodedInstruction != null) {
+				System.out.println("	" + "Output: ");
+				isJumped = execute(decodedInstruction);
+				decodedInstruction = null;
+			}
+
+			System.out.println();
+			System.out.println("Decode Stage:");
+			System.out.println("	" + "Input: " + fetchedInstruction);
+			if (fetchedInstruction != null) {
 				decodedInstruction = decode(fetchedInstruction);
-			if(fetchedInstruction == null)
-				decodedInstruction =null;
+				fetchedInstruction = null;
+			}
+			System.out.println("	" + "Output: " + Arrays.toString(decodedInstruction));
+			System.out.println();
+			System.out.println("Fetch Stage:");
+			System.out.println("	" + "Input: " + PC);
 			fetchedInstruction = fetch();
-			// memory ( ) ;
-			// writeback ( ) ;
+			System.out.println("	" + "Output: " + fetchedInstruction);
+
+			System.out.println();
+
+			System.out.println("Registers 		: " + Arrays.toString(registers));
+			System.out.println("Data Memory		: " + Arrays.toString(dataMemory));
+			System.out.println("Instruction Memory	: " + Arrays.toString(instructionMemory));
+
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			if(isJumped)
+			{
+				fetchedInstruction = null;
+				decodedInstruction = null;
+			}
 		}
+
 	}
 
 	public int getBit(byte b, int position) {
@@ -262,46 +326,23 @@ public class Processor {
 	}
 
 	public static void main(String[] args) {
-//	    byte x = -128;
-//		System.out.println(Byte.MAX_VALUE);
-//		System.out.println(Integer.toBinaryString(x).substring(24));
-//		String x = "1000" + "000100" + "001011";
-//		int z = Integer.parseInt(x, 2);
-//		short y = (short)z;
-//		System.out.println(Integer.toBinaryString(z));
-//		System.out.println(Integer.toBinaryString(y).substring(16));
-//		byte x = -127;
-//		System.out.println(((byte)x<<1));
-//		"7" --> 7 --> "111"
-//		System.out.println(Integer.toBinaryString(Integer.parseInt("7")));
 
 		Processor processor = new Processor();
 		try {
-			processor.parse("test.txt");
+			processor.parse("test2.txt");
 		}
 		catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("Wrong path");
 		}
 		processor.run();
+		
+		
+
 //		for (String x : processor.instructionMemory)
 //			if (x != null) {
 //				System.out.println(x.substring(0, 4) + " | " + x.substring(4, 10) + " | " + x.substring(10, 16));
 //				System.out.println(x);
 //			}
-
-//		Byte x = (byte) Integer.parseUnsignedInt("10000000",2);
-//		String y = "11110000";
-//		x = (byte)Integer.parseUnsignedInt(y, 2);
-//		byte y = 2;
-//		System.out.println(Integer.toBinaryString(y&0xff));
-//		
-//		String x = "-5";
-//		String y = "5";
-//		System.out.println(processor.extend(Integer.toBinaryString(Integer.parseInt(x)&0xff), 8));
-//		System.out.println(processor.extend(Integer.toBinaryString(Integer.parseInt(y)&0xff), 8));
-
-//		byte x = (byte) (128 + 2);
-//		System.out.println(x < 0);
 
 	}
 }
